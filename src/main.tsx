ff33d383d768d5e96c9b0180c2d56817b57697d2
Roman Plugaru <roman.plugaru@internal.ebs.md>
2025-01-16 17:25:04 +0200

    fix: config vite

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,10 @@ import 'leaflet/dist/leaflet.css'
 import 'react-leaflet-markercluster/styles'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+export function renderMapWidget() {
+  return createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,23 @@ import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    lib: {
+      entry: 'src/main.tsx',
+      name: 'VetMapWidget',
+      fileName: (format) => `map-widget.${format}.js`,
+      formats: ['umd']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
   resolve: {
     alias: [
       {
@@ -11,5 +28,8 @@ export default defineConfig({
         replacement: '/src'
       }
     ]
+  },
+  define: {
+    'process.env': {}
   }
 })
